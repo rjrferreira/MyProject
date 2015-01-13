8aa75dd0f3f38677cebb54135dd05f2e6ab6ae49
Rodrigo Ferreira <rjrferreira@gmail.com>
2015-01-13 18:44:20 +0000

    Doorkeeper_test

---
+++ doorkeeper_test/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DoorkeeperTest::Application.config.secret_key_base = '17197bbf9929e084cdd7b086b3ad15fef3d3a0f28254cc3e3dd765123b497e8c6cda32a6c587da17145fc7498a5dd90e8d9b6719e960fc9431fde412f251d5cc'
---
+++ doorkeeper_test/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DoorkeeperTest::Application.config.session_store :cookie_store, key: '_doorkeeper_test_session'
